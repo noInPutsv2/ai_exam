8e7217decc980d7136827f8eafb6ca81921a97e6
PhoenixPhoebe <linewienke+github@gmail.com>
2024-06-07 21:39:29 +0200

    Vector database lavet om til at køre uden docker

--- Vector_database_functions.py
+++ Vector_database_functions.py
@@ -2,16 +2,16 @@ from langchain.vectorstores import Chroma
 import chromadb
 from chromadb.config import Settings
 
-chroma_client = chromadb.HttpClient(settings=Settings(allow_reset=True))
+#chroma_client = chromadb.HttpClient(settings=Settings(allow_reset=True))
 
 
 #Creates new collection and adds documents to it
 def Create(docs, collection_name, embedding_function, data_directory):
-    document_collection = chroma_client.create_collection(name=collection_name)
+    #document_collection = chroma_client.create_collection(name=collection_name)
     ids = [str(i) for i in range(1, len(docs) + 1)]
     #db = Chroma(client=chroma_client, collection_name=collection_name, persist_directory=data_directory, embedding_function=embedding_function)
-    Chroma.from_documents(docs, embedding_function, client=chroma_client, collection_name=collection_name, persist_directory=data_directory)
-    return "Database created and populated with " + str(document_collection.count()) + " documents"
+    Chroma.from_documents(docs, embedding_function, collection_name=collection_name, persist_directory=data_directory)
+    #return "Database created and populated with " + str(document_collection.count()) + " documents"
 
 ### Adds documents to an existing collection
 def Add_docs(doc, collection_name, embedding_function, data_directory):
@@ -20,7 +20,7 @@ def Add_docs(doc, collection_name, embedding_function, data_directory):
 
 ### Get the whole database
 def get_db(collection_name, data_directory, embedding_function):
-    db = Chroma(persist_directory = data_directory, collection_name=collection_name, client=chroma_client, embedding_function = embedding_function)
+    db = Chroma(persist_directory = data_directory, collection_name=collection_name, embedding_function = embedding_function)
     return db
 
 def update(id, doc, collection_name, data_directory, embedding_function):
@@ -39,6 +39,6 @@ def delete_last_index(collection_name, data_directory, embedding_function):
    return "Document deleted"
 
 ### Deletes the whole collection
-def Delete_Collection(collection_name):
-    chroma_client.delete_collection(name=collection_name)
-    return "Collection deleted"
+#def Delete_Collection(collection_name):
+ #   chroma_client.delete_collection(name=collection_name)
+  #  return "Collection deleted"
